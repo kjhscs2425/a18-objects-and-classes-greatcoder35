48bc2e6d639c05832fee2fad40116c8a829791ca
Jordan Nash <jordangabrielnash@gmail.com>
2025-05-07 11:54:19 -0700

    q3 done

--- q3.py
+++ q3.py
@@ -19,9 +19,16 @@ print(ella)
 # Your class should have its own `__str__` method that returns a string.
 #### YOUR CODE HERE ####
 
+class Dog:
+    """a dog"""
+    def __init__(self, name):
+        self.name = name
+    def __str__(self):
+        return f"<a 🐶Dog named {self.name}>"
+    def speak(self):
+        print(f"{self.name} says woof!🐶")
 
 # Make a new object of class `Dog` and print it out
 #### YOUR CODE HERE ####
-
-
-
+neon = Dog("Neon")
+print(neon)
